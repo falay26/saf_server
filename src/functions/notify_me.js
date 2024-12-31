3527686d4303e97a73ea42e8bbe19d813b679fd9
Mehmet Serdar Falay <vodece@gmail.com>
2024-12-31 14:46:17 +0300

    firebase secured

--- src/functions/notify_me.js
+++ src/functions/notify_me.js
@@ -1,21 +1,39 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../firebase/main");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: process.env.JSON_1,
+    project_id: process.env.JSON_2,
+    private_key_id: process.env.JSON_3,
+    private_key:
+      "-----BEGIN PRIVATE KEY-----\n" +
+      process.env.JSON_4 +
+      "\n-----END PRIVATE KEY-----\n",
+    client_email: process.env.JSON_5,
+    client_id: process.env.JSON_6,
+    auth_uri: process.env.JSON_7,
+    token_uri: process.env.JSON_8,
+    auth_provider_x509_cert_url: process.env.JSON_9,
+    client_x509_cert_url: process.env.JSON_10,
+    universe_domain: process.env.JSON_11,
+  }),
 });
 
 const NM = (message) => {
-  var notif_message = {
-    token:
-      "evOegZ4sz07tmq1Fph6XaR:APA91bHEK4tpEa_sH1NNxjv15eWt2qX90ASUOPG6SHEpv1-BNrQWO-5RYWnxerzz4-xms7cWtGBTzyvr8lbgzJgPt4UDTV0eZW9LF0vT4L5r5Qpt0fkDPrLU6fXZpReSVPL7DkK5pPDE",
-    notification: {
-      title: "SBF",
-      body: message,
-    },
-  };
+  try {
+    var notif_message = {
+      token:
+        "evOegZ4sz07tmq1Fph6XaR:APA91bHEK4tpEa_sH1NNxjv15eWt2qX90ASUOPG6SHEpv1-BNrQWO-5RYWnxerzz4-xms7cWtGBTzyvr8lbgzJgPt4UDTV0eZW9LF0vT4L5r5Qpt0fkDPrLU6fXZpReSVPL7DkK5pPDE",
+      notification: {
+        title: "SBF",
+        body: message,
+      },
+    };
 
-  admin.messaging().send(notif_message);
+    admin.messaging().send(notif_message);
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 module.exports = NM;
